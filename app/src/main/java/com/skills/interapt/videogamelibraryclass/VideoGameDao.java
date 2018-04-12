5e57499def971ed177be8d4e1ec1e54ff3b6c6c3
Crystal Adkins <crystaladkins09@gmail.com>
2018-04-12 15:24:48 -0400

    Initial commit

---
+++ app/src/main/java/com/skills/interapt/videogamelibraryclass/VideoGameDao.java
@@ -0,0 +1,32 @@
+package com.skills.interapt.videogamelibraryclass;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface VideoGameDao {
+
+
+    //Allow us to get all video games
+    @Query("SELECT * FROM videogame")
+    List<VideoGame> getVideoGames();
+
+    //Allows us to add a single game to the list
+    @Insert
+    void addVideoGame(VideoGame videoGame);
+
+    //Allows us to update the values of an existing game
+    @Update
+    void updateVideoGame(VideoGame videoGame);
+
+    //Allows us to delete a game from the library
+    @Delete
+    void deleteVideoGame(VideoGame videoGame);
+
+
+}
