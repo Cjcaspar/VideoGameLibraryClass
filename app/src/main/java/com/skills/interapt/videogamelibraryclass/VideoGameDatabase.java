32a02b6b55ae5bf07817fff96b4d94b8c4f961c0
Crystal Adkins <crystaladkins09@gmail.com>
2018-04-16 14:17:03 -0400

    Added VideoGameDatabase and VideoGameApplication classes.  Updated manifest with application class.

---
+++ app/src/main/java/com/skills/interapt/videogamelibraryclass/VideoGameDatabase.java
@@ -0,0 +1,16 @@
+package com.skills.interapt.videogamelibraryclass;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+@Database(version = 1, entities = VideoGame.class)
+@TypeConverters(DateConverter.class)
+abstract class VideoGameDatabase extends RoomDatabase {
+
+
+    public abstract VideoGameDao videoGameDao();
+
+
+
+}
